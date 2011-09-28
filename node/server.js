fb18b2b233240f87ff0b498c52852871a099f892
Ben Lindsey <ben@carbonfive.com>
2011-09-28 14:09:59 -0700

    added connection: close and sent the user-agent as the body

--- node/server.js
+++ node/server.js
@@ -1,7 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  //console.log( JSON.stringify( req.headers ) );
-  var content = '<html><body><h1>Hello World</h1></body></html>\r\n';
-  res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length' : content.length.toString() });
+  var content = '<html><body><h1>' + req.headers['user-agent'] + '</h1></body></html>\r\n';
+  res.writeHead(200, {'Connection' : 'close', 'Content-Type': 'text/html', 'Content-Length' : content.length.toString() });
   res.end(content);
 }).listen(8081);
