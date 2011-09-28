b15e8d5a5ac8ee30e0487811f05887b491fe45e3
Michael Wynholds <mike@carbonfive.com>
2011-09-28 09:40:26 -0700

    Move files to subdirs

---
+++ node/server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  //console.log( JSON.stringify( req.headers ) );
+  var content = '<html><body><h1>Hello World</h1></body></html>';
+  res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length' : content.length.toString() });
+  res.end(content);
+}).listen(8080);
