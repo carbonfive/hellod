1031c91efffd2ee753561f381ee096632f2d5da9
Michael Wynholds <mike@carbonfive.com>
2011-09-28 11:09:29 -0700

    Add CRLF

--- node/server.js
+++ node/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   //console.log( JSON.stringify( req.headers ) );
-  var content = '<html><body><h1>Hello World</h1></body></html>';
+  var content = '<html><body><h1>Hello World</h1></body></html>\r\n';
   res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length' : content.length.toString() });
   res.end(content);
 }).listen(8081);
