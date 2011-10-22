fc48486a6373bf9c9d53bd90ccb56976b9704380
Michael Wynholds <mike@carbonfive.com>
2011-10-22 00:23:43 +0000

    Better test runner

--- test-all.sh
+++ test-all.sh
@@ -1,84 +1,57 @@
 #!/bin/bash
 
-short="-n 10000 -c 50"
-long="-n 50000 -c 200"
-
 source "$HOME/.rvm/scripts/rvm"
+
+run()
+{
+  local flavor=$1
+  local desc=$2
+  local version1=$3
+  local version2=$4
+  local rvm=$5
+  local java=$6
+
+  if [ "$rvm" != "" ]; then
+    rvm $rvm
+  fi
+
+  if [ "$java" != "" ]; then
+    sudo update-alternatives --set java $java > /dev/null
+  fi
+
+  ./hellod start $flavor > /dev/null
+  echo $desc
+  echo "-------------"
+  $version1
+
+  if [ "$version2" != "" ]; then
+    $version2
+  fi
+
+  echo ""
+  sleep 5
+  ./hellod test $flavor -n 10000 -c 50 > /dev/null
+  ./hellod test $flavor -n 10000 -c 50 > /dev/null
+
+  ./hellod test $flavor -n 10000 -c 50
+  ./hellod test $flavor -n 50000 -c 200
+  ./hellod stop $flavor > /dev/null
+
+  echo "" 
+}
 
 ./hellod stop all > /dev/null
 
-rvm 1.9.2@hellod
-./hellod start ruby > /dev/null
-echo "Ruby 1.9.2" 
-echo "----------" 
-echo `ruby -v`
-echo ""
-./hellod test ruby $short 
-./hellod test ruby $long
-./hellod stop ruby > /dev/null
-echo ""
+OPENJDK='/usr/lib/jvm/java-6-openjdk/jre/bin/java'
+SUNJDK='/usr/lib/jvm/java-6-sun/jre/bin/java'
 
-rvm 1.9.3@hellod
-./hellod start ruby > /dev/null
-echo "Ruby 1.9.3"
-echo "----------"
-echo `ruby -v`
-echo ""
-./hellod test ruby $short
-./hellod test ruby $long
-./hellod stop ruby > /dev/null
-echo ""
-
-rvm jruby@hellod
-./hellod start ruby > /dev/null
-sleep 10
-echo "JRuby 1.6.4"
-echo "-----------"
-echo `ruby -v`
-echo ""
-./hellod test ruby $short
-./hellod test ruby $long
-./hellod stop ruby > /dev/null
-echo ""
-
-./hellod start go > /dev/null
-echo "go"
-echo "-----"
-echo `6g -V`
-echo ""
-./hellod test go $short
-./hellod test go $long
-./hellod stop go > /dev/null
-echo ""
-
-./hellod start node > /dev/null
-echo "Node.js"
-echo "-------"
-echo `node -v`
-echo ""
-./hellod test node $short
-./hellod test node $long
-./hellod stop node > /dev/null
-echo ""
-
-./hellod start clj-aleph > /dev/null
-sleep 10
-echo "Clojure (with Aleph)"
-echo "--------------------"
-echo `lein version`
-echo ""
-./hellod test clj-aleph $short
-./hellod test clj-aleph $long
-./hellod stop clj-aleph > /dev/null
-echo ""
-
-./hellod start clj-jetty > /dev/null
-sleep 10
-echo "Clojure (with Jetty)"
-echo "--------------------"
-echo `lein version`
-echo ""
-./hellod test clj-jetty $short
-./hellod test clj-jetty $long
-./hellod stop clj-jetty > /dev/null
-echo ""
+run 'ruby' 'Ruby 1.9.2' 'ruby -v' '' '1.9.2@hellod' ''
+run 'ruby' 'Ruby 1.9.3' 'ruby -v' '' '1.9.3@hellod' ''
+run 'ruby' 'JRuby 1.6.4 - Sun JDK' 'ruby -v' 'java -version' 'jruby@hellod' $SUNJDK
+run 'ruby' 'JRuby 1.6.4 - OpenJDK' 'ruby -v' 'java -version' 'jruby@hellod' $OPENJDK
+run 'go' 'Go' '6g -V' '' '' ''
+run 'node' 'Node.js' 'node -v' '' '' ''
+run 'clj-aleph' 'Clojure / Aleph - Sun JDK' 'lein version' 'java -version' '' $SUNJDK
+run 'clj-aleph' 'Clojure / Aleph - OpenJDK' 'lein version' 'java -version' '' $OPENJDK
+run 'clj-jetty' 'Clojure / Ring - Sun JDK' 'lein version' 'java -version' '' $SUNJDK
+run 'clj-jetty' 'Clojure / Ring - OpenJDK' 'lein version' 'java -version' '' $OPENJDK
